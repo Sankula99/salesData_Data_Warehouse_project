59c15539b4fe50c55561a878c27b211b8d5da278
Sankula99 <148407344+Sankula99@users.noreply.github.com>
2025-06-04 11:17:16 +1000

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,15 @@
+/*
+==========================================
+CREATE SCHEMAS
+==========================================
+SCRIPT PURPOSE:
+This script creates schemas (Gold, Silver and Bronze) to be used for the ETL process
+*/
+--CREATE GOLD SCHEMA
+	CREATE SCHEMA GOLD;
+
+--CREATE SILVER SCHEMA
+	CREATE SCHEMA SILVER;
+
+--CREATE BRONZE SCHEMA
+	CREATE SCHEMA BRONZE;
